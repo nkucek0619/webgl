60ff757b129c954aab068004b201bd0f325fbf80
Nicholas Kucek <ndk35@uakron.edu>
2022-02-23 21:26:38 -0500

    Add starter files

---
+++ PA2-Starter/cubev.js
@@ -0,0 +1,159 @@
+"use strict";
+
+var canvas;
+var gl;
+
+var numVertices  = 72; //2x6x2x3
+
+var axis = 0;
+var xAxis = 0;
+var yAxis =1;
+var zAxis = 2;
+var theta = [ 45, 45, 45 ];
+var thetaLoc;
+
+    var vertices = [
+        vec3( -0.25, -0.25,  0.25 ),
+        vec3( -0.25,  0.25,  0.25 ),
+        vec3(  0.25,  0.25,  0.25 ),
+        vec3(  0.25, -0.25,  0.25 ),
+        vec3( -0.25, -0.25, -0.25 ),
+        vec3( -0.25,  0.25, -0.25 ),
+        vec3(  0.25,  0.25, -0.25 ),
+        vec3(  0.25, -0.25, -0.25 ),
+        vec3(  1.25,  1.25, -1.25 ),
+        vec3(  1.25, -1.25, -1.25 ),  
+	vec3( -0.25, -0.25,  .5 ),
+        vec3( -0.25,  0.25,  .5 ),
+        vec3(  0.25,  0.25,  .5 ),
+        vec3(  0.25, -0.25,  .5 ),
+        vec3( -0.25, -0.25, .75 ),
+        vec3( -0.25,  0.25, .75 ),
+        vec3(  0.25,  0.25, .75 ),
+        vec3(  0.25, -0.25, .75 )   ];
+
+    var vertexColors = [
+        vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+        vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+        vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+        vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+        vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+        vec4( 0.0, 1.0, 1.0, 1.0 ),   // cyan
+        vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+        vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+        vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+        vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+        vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+        vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+        vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+        vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+    ];
+
+// indices of the 12x2 triangles that compise the cube
+
+var indices = [
+    1, 0, 3,
+    3, 2, 1,
+    2, 3, 7,
+    7, 6, 2,
+    3, 0, 4,
+    4, 7, 3,
+    6, 5, 1,
+    1, 2, 6,
+    4, 5, 6,
+    6, 7, 4,
+    5, 4, 0,
+    0, 1, 5,
+    11, 10, 13,
+    13, 12, 11,
+    12, 13, 17,
+    17, 16, 12,
+    13, 10, 14,
+    14, 17, 13,
+    16, 15, 11,
+    11, 12, 16,
+    14, 15, 16,
+    16, 17, 14,
+    15, 14, 10,
+    10, 11, 15
+
+];
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);;
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // array element buffer
+
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+    // color array atrribute buffer
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    // vertex array attribute buffer
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
+    //event listeners for buttons
+
+    document.getElementById( "xButton" ).onclick = function () {
+        axis = xAxis;
+    };
+    document.getElementById( "yButton" ).onclick = function () {
+        axis = yAxis;
+    };
+    document.getElementById( "zButton" ).onclick = function () {
+        axis = zAxis;
+    };
+
+
+    render();
+}
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    //theta[axis] += 2.0;
+    gl.uniform3fv(thetaLoc, theta);
+
+
+    gl.drawElements( gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0 );
+
+    requestAnimFrame( render );
+}
